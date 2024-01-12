89e4ae457ac2bdc018a112903f01cd61a6dcd75e
maxixking66 <maxixking66@gamil.com>
2024-01-12 08:07:12 +0330

    remove extra classes

--- src/main/java/ir/maktabsharif101/springboot/firstspringboot/FirstSpringBootApplication.java
+++ src/main/java/ir/maktabsharif101/springboot/firstspringboot/FirstSpringBootApplication.java
@@ -1,44 +1,13 @@
 package ir.maktabsharif101.springboot.firstspringboot;
 
-import ir.maktabsharif101.springboot.firstspringboot.domain.Address;
-import ir.maktabsharif101.springboot.firstspringboot.repository.AddressRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
 public class FirstSpringBootApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(FirstSpringBootApplication.class, args);
-
-        AddressRepository addressRepository = run.getBean(
-                AddressRepository.class
-        );
-
-        Page<Address> page = addressRepository.findAll(
-                PageRequest.of(
-                        0, 4,
-                        Sort.by("customerId")
-                )
-        );
-
-        System.out.println(
-                "page totalElements: " + page.getTotalElements()
-        );
-
-        System.out.println(
-                "page totalPages: " + page.getTotalPages()
-        );
-
-        System.out.println(
-                "page content: " + page.getContent().get(0).getId()
-        );
-
-
+        SpringApplication.run(FirstSpringBootApplication.class, args);
     }
 
 }
